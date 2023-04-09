4a6aa4f52edc8f3c4372cabf7f3c79c91380da50
ralph <ralphwannabee@yahoo.com>
2023-04-08 19:56:29 -0700

    first commit

---
+++ message.js
@@ -0,0 +1,32 @@
+const birthday =(name) => {
+    const randomMessage = Math.floor(Math.random() * 13);
+    switch (randomMessage){
+        case 1:
+            return `Hello ${name} lets Fuck daniela`;
+        case 2:
+            return `Hello ${name} today is a good day`;
+        case 3:
+            return `Hello ${name} you are a winner`;
+        case 4:
+            return `Hello ${name} go fuck a tranny`;
+        case 5:
+            return `Hello ${name} love you`;
+        case 6:
+            return `Hello ${name} lets get some prostitutes`;
+        case 7:
+            return `Hello ${name} fuck daniela so hard`;
+        case 8:
+            return `Hello ${name} i don't know`;
+        case 9:
+            return `Hello ${name} still I don't know`;
+        case 10:
+            return `Hello ${name} huh?`;
+        case 11:
+            return `Hello ${name} 7-11`;
+        case 12:
+            return `Hello ${name} uggh`;
+    }
+}
+
+
+console.log(birthday('ralph'));
